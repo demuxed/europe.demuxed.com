a548981a7924bd65584d488cddd1eb93d8b7f210
Phil Cluff <philcluff@googlemail.com>
2019-11-13 08:12:14 +0000

    Handle errors, feedback while sending

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,36 +1,46 @@
 // This is where it all goes :)
-(function (document) {
-  console.log('woo');
-  function ready(callback){
+(function(document) {
+  console.log("woo");
+  function ready(callback) {
     // in case the document is already rendered
-    if (document.readyState!='loading') callback();
+    if (document.readyState != "loading") callback();
     // modern browsers
-    else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
+    else if (document.addEventListener)
+      document.addEventListener("DOMContentLoaded", callback);
     // IE <= 8
-    else document.attachEvent('onreadystatechange', function(){
-        if (document.readyState=='complete') callback();
-    });
+    else
+      document.attachEvent("onreadystatechange", function() {
+        if (document.readyState == "complete") callback();
+      });
   }
 
   ready(() => {
-    $('form#interest').submit((e) => {
+    $("form#interest").submit(e => {
       e.preventDefault();
 
-      const $email = $('.email-input');
-      const $button = $('.submit-button');
+      const $email = $(".email-input");
+      const $button = $(".submit-button");
+
+      $email.attr("disabled", true);
+      $button.attr("disabled", true);
+      $button.val("Sending...");
+
       $.ajax({
-        type: 'POST',
-        url: 'https://demuxed-europe-serverless-philcluff.demuxed.now.sh/api/add-email',
+        type: "POST",
+        url:
+          "https://demuxed-europe-serverless-philcluff.demuxed.now.sh/api/add-email",
         data: JSON.stringify({ email: $email.val() }),
-        dataType: 'json',
-        success: (data) => {
-          $email.attr('disabled', true);
-          $button.attr('disabled', true);
+        dataType: "json",
+        success: data => {
           $button.val("We'll be in touch!");
         },
-        failure: console.log,
+        failure: data => {
+          console.log(data);
+          $email.attr("disabled", false);
+          $button.attr("disabled", false);
+          $button.val("Failed, try again?");
+        }
       });
     });
   });
-
 })(document);
